c3a388c80ce54e6f3e0477707d9079646f2d41c5
Parzi41 <kolyatishkevich@gmail.com>
2023-11-29 20:08:59 +0200

    Added method to get top 10 players, fixed background image

--- Game/src/main/java/com/example/game/controllers/LeaderboardController.java
+++ Game/src/main/java/com/example/game/controllers/LeaderboardController.java
@@ -7,6 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+
 @Controller
 public class LeaderboardController {
     @Autowired
@@ -15,7 +21,19 @@ public class LeaderboardController {
     @GetMapping("/leaderboard")
     public String leaderboard(Model model) {
         Iterable<Leaderboard> leaders = leaderRepo.findAll();
-        model.addAttribute("leaders", leaders);
+
+        List<Leaderboard> topElements = getTopElements(leaders, 10);
+
+        model.addAttribute("leaders", topElements);
         return "leaderboard";
+    }
+
+    private static List<Leaderboard> getTopElements(Iterable<Leaderboard> iterable, int count) {
+        List<Leaderboard> list = StreamSupport.stream(iterable.spliterator(), false)
+                .collect(Collectors.toList());
+
+        list.sort(Comparator.comparing(Leaderboard::getScore).reversed());
+
+        return list.subList(0, Math.min(count, list.size()));
     }
 }
