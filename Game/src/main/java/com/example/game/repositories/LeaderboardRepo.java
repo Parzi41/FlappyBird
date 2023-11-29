5bdf2044e78a938ca2380d625354fb8f2253415c
Parzi41 <kolyatishkevich@gmail.com>
2023-11-29 15:47:59 +0200

    Added leaderboard page, created basic table template

--- Game/src/main/java/com/example/game/repositories/LeaderboardRepo.java
+++ Game/src/main/java/com/example/game/repositories/LeaderboardRepo.java
@@ -3,5 +3,7 @@ package com.example.game.repositories;
 import com.example.game.models.Leaderboard;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface LeaderboardRepo extends CrudRepository<Leaderboard, Long> {
 }
